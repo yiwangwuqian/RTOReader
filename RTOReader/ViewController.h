8d41f1c0e7ee35670d8808141f66d20fde12d4dd
yiwangwuqian <670700683@qq.com>
2020-03-25 21:53:37 +0800

    Initial Commit

---
+++ RTOReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RTOReader
+//
+//  Created by ghy on 2020/3/25.
+//  Copyright © 2020 ghy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RTOReader/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RTOReader
+//
+//  Created by ghy on 2020/3/25.
+//  Copyright © 2020 ghy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
